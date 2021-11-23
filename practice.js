9c2d6e3a5343b7dca1d2d282fda599f30959c6fe
anwar_moazam <anwar.palimarwar@gmail.com>
2021-11-23 23:18:01 +0530

    Edabit practice

--- practice.js
+++ practice.js
@@ -14,7 +14,6 @@ function sevenBoom(arr){
     for(let i=0;i<arr.length;i++){
         if(arr[i].toString().length>1){
             let subArr = [...arr[i].toString()];
-            console.log(subArr);
             if(subArr.includes('7',0)){
                 return 'Boom!';
             }
@@ -24,8 +23,23 @@ function sevenBoom(arr){
     return "there is no 7 in the array";
 }
 
+function sevenBoom2(arr){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i].toString().length>1){
+            let subArr = [...arr[i].toString()].map((x)=>Number(x));
+            for(let j=0;j<subArr.length;j++){
+                if(subArr[j]===7){
+                    return 'Boom!';
+                }
+            }
+        } else if(arr[i]===7)
+                return 'Boom!';
+    }
+    return "there is no 7 in the array";
+}
 
-// console.log(sevenBoom([2, 3, 4, 5, 6857]));
+
+console.log(sevenBoom2([172, 3, 4, 5, 6857]));
 // console.log(sevenBoom([8, 6, 33, 105415415407]));
 // console.log(sevenBoom([2, 55, 60, 97, 86]));
 // console.log(sevenBoom([2, 55, 7, 60, 9, 758]));
@@ -47,3 +61,62 @@ console.log(oddishOrEvenish(373));
 console.log(oddishOrEvenish(4433));
 console.log(oddishOrEvenish(121));
 console.log(oddishOrEvenish(41));
+
+/*
+Combinations
+Create a function that takes a variable number of arguments, each argument representing the number of items in a group, and returns the number of permutations (combinations) of items that you could get by taking one item from each group.
+
+Examples
+combinations(2, 3) ➞ 6
+
+combinations(3, 7, 4) ➞ 84
+
+combinations(2, 3, 4, 5) ➞ 120
+*/
+
+function combinations(){
+    let result = [];
+    for(let i=0; i<arguments.length; i++){
+        result.push(arguments[i]);
+    }
+    return result.reduce((acc, curr) => {return acc * curr;});
+}
+
+console.log(combinations(4,4,3));
+
+// function allTruthy(arr) {
+//     for(let i=0; i<arr.length; i++) {
+//         if(arr[i] !== true){
+//             return false;
+//         } else {
+//             return true;
+//         }
+//     }
+// }  
+
+
+function allTruthy() {
+    for(let i=0; i<arguments.length; i++){
+        if(arguments[i] == false){
+            return false;
+        }
+    }
+    return true;
+}
+
+// console.log(allTruthy(true, true, true));
+// console.log(allTruthy(true, true, false));
+
+function allTruthy1(arr) {
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] == false){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(allTruthy1([true, true, true]));
+console.log(allTruthy1([true, true, false]));
+console.log(allTruthy1([false, true, true, true]));
+
